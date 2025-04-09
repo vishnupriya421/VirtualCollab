07e4f791657eb36abb3d4a00963697e84f61c541
vishnupriya421 <2000031988cse@gmail.com>
2025-04-09 10:42:13 +0530

    Add files via upload

---
+++ MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microservices.TaskManagement.Models.Dto;
+
+namespace Microservices.TaskManagement
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<TaskManagementDto, Task>();
+                config.CreateMap<Task, TaskManagementDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
